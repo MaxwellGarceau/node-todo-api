c50cdc55dbaf07d39da96512176d8c9446959d07
Max Garceau <maxwellgarceau@gmail.com>
2018-07-17 09:19:55 -0500

    Fetching data from Mongo.db

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -25,17 +25,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Max',
-    age: 26,
-    location: 'Nashville, TN'
-  }, (error, result) => {
-    if (error) {
-      return console.log('Unable to insert challenge exercise');
-    }
+  // db.collection('Users').insertOne({
+  //   name: 'Max',
+  //   age: 26,
+  //   location: 'Nashville, TN'
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert challenge exercise');
+  //   }
 
-    console.log(result.ops[0]._id.getTimestamp());
-  });
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   db.close();
 });
---
+++ playground/mongodb-find.js
@@ -0,0 +1,23 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+  if (error) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (error) => {
+  //   console.log('Unable to fetch todos', error);
+  // });
+
+  db.collection('Users').find({ name: 'Max' }).toArray().then((user) => {
+    console.log(user);
+  }, (error) => {
+    console.log(`Didn't work`);
+  });
+
+  // db.close();
+});
