33c62dbd4207701f654c1059f130ba58c066d71b
Max Garceau <maxwellgarceau@gmail.com>
2018-07-19 08:35:22 -0500

    Validators, Types, and Defaults

--- server/server.js
+++ server/server.js
@@ -5,34 +5,63 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-const newTodo = new Todo({
-  text: 'Cook dinner'
+// const newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+// const myTodo = new Todo({
+//   text: 'Validators, Types, and Defaults'
+// });
+
+// myTodo.save().then((doc) => {
+//   console.log('Saved my todo', doc);
+// }, (e) => {
+//   console.log(`Didn't save my todo`);
+// });
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    minlength: 1,
+    required: true
+  }
 });
 
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
+const myAdminProfile = new User({
+  name: 'Max Garceau',
+  email: 'maxgarceau@comcast.net'
+});
+
+myAdminProfile.save().then((doc) => {
+  console.log('Saved: ', doc);
 }, (e) => {
-  console.log('Unable to save todo');
-});
-
-const myTodo = new Todo({
-  text: 'My todo',
-  completed: false,
-  completedAt: null
-});
-
-myTodo.save().then((doc) => {
-  console.log('Saved my todo', doc);
-}, (e) => {
-  console.log(`Didn't save my todo`);
+  console.log(`Didn't save`);
 });
