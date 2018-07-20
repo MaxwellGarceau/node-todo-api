e6a4d4883771bf7abde25f14391a85aa70c6d693
Max Garceau <maxwellgarceau@gmail.com>
2018-07-20 09:13:25 -0500

    Set up GET /todos Route

--- server/server.js
+++ server/server.js
@@ -22,22 +22,13 @@ app.post('/todos', (req, res) => {
   });
 });
 
-// const todoFunc = (endpoint) => {
-//   app.post(endpoint, (req, res) => {
-    
-//     const todo = new Todo({
-//       text: req.body.text
-//     });
-
-//     todo.save().then((doc) => {
-//       res.send(doc);
-//     });
-//   }, (e) => {
-//     res.status(400).send(e);
-//   });
-// };
-
-
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos });
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
