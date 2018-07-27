a40cec0357f229e52def63df90473c0c6f5dc3a4
Max Garceau <maxwellgarceau@gmail.com>
2018-07-27 09:21:36 -0500

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -141,3 +141,12 @@ app.listen(port, () => {
 module.exports = {
   app
 };
+
+// DELETE /users/me/token
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
